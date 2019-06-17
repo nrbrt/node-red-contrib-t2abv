08c32a49670f51c5ef0dada324cdd9fdede30749
norbertk <norbertk@lavaleriana.net>
2019-06-17 10:11:24 +0200

    made msg property for temperature configurable

--- tempoffset.js
+++ tempoffset.js
@@ -2,10 +2,11 @@ module.exports = function(RED) {
     function TempOffsetNode(config) {
         RED.nodes.createNode(this,config);
         this.contextglobalvariable = config.contextglobalvariable;
+	this.messageproperty = config.messageproperty;
         var node = this;
         node.on('input', function(msg) {
             var offset = this.context().global.get(this.contextglobalvariable)||0;
-            var temp = parseFloat(msg.payload);
+            var temp = parseFloat(msg[this.messageproperty]);
 	    msg.payload = temp + offset;
             node.send(msg);
         });
